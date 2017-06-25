817066a246135df5d66834d96afd8d27d888c098
Lauren Wolfe <lawolfe.dev@gmail.com>
2017-06-25 15:01:23 -0700

    +Added  styled 'submit' and 'reset' buttons +Added <hr>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # changeTextWithJavascript
 +Learning the basics of Javascript by changing the text of an HTML page.
 +Solidifying knowledge of HTML and CSS
++Become more familiar with Git and VC
 +It's the FUNDAMENTALS!
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Change heading
 var headline = document.getElementById("heading");
 headline.onclick = function(){
-    headline.innerHTML = "You did it!"
+    headline.innerHTML = "You did it!";
 };
+
 // Change paragraph text
 var txt = document.getElementById("text");
 txt.onclick = function(){
     txt.innerHTML = prompt("What would you like to say?");
+};
+
+// Buttons !NOT WORKING!
+var button = document.getElementById("submit");
+button.onclick = function(){
+    var userTxt = document.getElementById("user-text");
+    var userGen = document.getElementById("user-generated");
+
+
 };
