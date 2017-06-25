08c9ede9753faea4bb46a690d9981d663d0ea178
Lauren Wolfe <lawolfe.dev@gmail.com>
2017-06-25 09:49:55 -0700

    Paragraph text changeable via prompt onclick

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+// Change heading
 document.getElementById("heading");
 heading.onclick = function(){
     heading.innerHTML = "I did it!"
+};
+// Change paragraph text
+document.getElementById("text");
+text.onclick = function(){
+    text.innerHTML = prompt("What text would you like to enter on this page?");
 };
