12fb73948d57d93a703564972e6baf719fcd8d3c
Lauren Wolfe <lawolfe.dev@gmail.com>
2017-06-25 09:59:35 -0700

    Added 'var headline' and 'var txt' to script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Change heading
-document.getElementById("heading");
-heading.onclick = function(){
-    heading.innerHTML = "I did it!"
+var headline = document.getElementById("heading");
+headline.onclick = function(){
+    headline.innerHTML = "I did it!"
 };
 // Change paragraph text
-document.getElementById("text");
-text.onclick = function(){
-    text.innerHTML = prompt("What text would you like to enter on this page?");
+var txt = document.getElementById("text");
+txt.onclick = function(){
+    txt.innerHTML = prompt("What text would you like to enter on this page?");
 };
