b24007cfa277afc6aecebf06c45b3247e16325d4
Lauren Wolfe <lawolfe.dev@gmail.com>
2017-06-25 12:29:05 -0700

    Change Text With JavaScript
    Practicing basics of HTML, CSS, and JavaScript by changing HTML text
    with JavaScript.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Change heading
 var headline = document.getElementById("heading");
 headline.onclick = function(){
-    headline.innerHTML = "I did it!"
+    headline.innerHTML = "You did it!"
 };
 // Change paragraph text
 var txt = document.getElementById("text");
 txt.onclick = function(){
-    txt.innerHTML = prompt("What text would you like to enter on this page?");
+    txt.innerHTML = prompt("What would you like to say?");
 };
